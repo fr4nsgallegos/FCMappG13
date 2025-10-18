2c8001f2021e08bdd3c7f3a897fced681f4a5151
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-10-17 20:26:23 -0500

    configuiracion para obtrener ingfo cuando el app eta en segundo plano

--- lib/utils/notification_service.dart
+++ lib/utils/notification_service.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 
+@pragma('vm:entry-point') //Permite que el código sea accesible nativamente
 class NotificationService {
   static FirebaseMessaging firebaseMessaging = FirebaseMessaging.instance;
 
@@ -8,6 +9,8 @@ class NotificationService {
     print("token: $token");
 
     FirebaseMessaging.onMessage.listen(_onMessage);
+
+    FirebaseMessaging.onBackgroundMessage(_onBackgroundMessage);
   }
 
   // Obtener info cuando el app esta abierto
@@ -17,5 +20,17 @@ class NotificationService {
     print(message.notification!.title);
     print(message.notification!.body);
     print("*******************************");
+  }
+
+  // Obtener cuando este en segundo plano
+  @pragma('vm:entry-point') //Permite que el código sea accesible nativamente
+  static Future<void> _onBackgroundMessage(RemoteMessage message) async {
+    if (message.notification != null) {
+      print("------------------------");
+      print(message.notification);
+      print(message.notification!.title);
+      print(message.notification!.body);
+      print("------------------------");
+    }
   }
 }
