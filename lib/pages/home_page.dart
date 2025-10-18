013937fc6354a4020149ba66df05435a029994bc
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-10-17 22:16:19 -0500

    exportando cont abla

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
 import 'package:path/path.dart' as path;
 
@@ -17,7 +18,28 @@ class HomePage extends StatelessWidget {
     pdf.addPage(
       pw.Page(
         build: (pw.Context context) {
-          return pw.Center(child: pw.Text("Hola esto es un pdf"));
+          return pw.Center(
+            child: pw.Column(
+              children: [
+                pw.Text(
+                  "Hola este es un pdf creado por Flutter! ",
+                  style: pw.TextStyle(
+                    fontSize: 30,
+                    fontWeight: pw.FontWeight.bold,
+                  ),
+                ),
+                pw.SizedBox(height: 32),
+                pw.Text("Factura"),
+                pw.Text("Cliente: Juanito Perez"),
+                pw.Text("Fecha:  ${DateTime.now()}"),
+                pw.Container(
+                  height: 16,
+                  width: 32,
+                  decoration: pw.BoxDecoration(color: PdfColor(1, 1, 1)),
+                ),
+              ],
+            ),
+          );
         },
       ),
     );
@@ -26,7 +48,36 @@ class HomePage extends StatelessWidget {
     final output = await getApplicationDocumentsDirectory();
 
     // Uso path para generar correctamente la ruta
-    final filePath = path.join(output.path, "example.pdf");
+    final filePath = path.join(output.path, "example1.pdf");
+    final file = File(filePath);
+
+    // Guardar el archivo
+    await file.writeAsBytes(await pdf.save());
+    print("pdf guardado en ${file.path}");
+    return file;
+  }
+
+  Future<File> generarTablaPdf() async {
+    final pdf = pw.Document();
+    pdf.addPage(
+      pw.Page(
+        build: (pw.Context context) {
+          return pw.Table.fromTextArray(
+            headers: ["Producto", "Cantidad", "Precio"],
+            data: [
+              ["Laptop", "1", "S/ 14500"],
+              ["Mouse", "2", "S/ 50"],
+              ["Teclado", "10", "S/ 300"],
+            ],
+          );
+        },
+      ),
+    );
+    // Obtener la ruta de almacenamiento local
+    final output = await getApplicationDocumentsDirectory();
+
+    // Uso path para generar correctamente la ruta
+    final filePath = path.join(output.path, "example_tabla.pdf");
     final file = File(filePath);
 
     // Guardar el archivo
@@ -48,6 +99,12 @@ class HomePage extends StatelessWidget {
                 generarPdf();
               },
               child: Text("Exportar a pdf"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                generarTablaPdf();
+              },
+              child: Text("Exportar a pdf con tabla "),
             ),
           ],
         ),
