563e5caba54b042080ae5e0353fb6a5055c61553
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-10-17 19:43:23 -0500

    get token mesagging

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:fcmappg13/firebase_options.dart';
 import 'package:fcmappg13/pages/home_page.dart';
+import 'package:fcmappg13/utils/notification_service.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await NotificationService.initMessaging();
   runApp(MaterialApp(home: HomePage()));
 }
---
+++ lib/utils/notification_service.dart
@@ -0,0 +1,10 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+class NotificationService {
+  static FirebaseMessaging firebaseMessaging = FirebaseMessaging.instance;
+
+  static initMessaging() async {
+    String token = await firebaseMessaging.getToken() ?? '-';
+    print("token: $token");
+  }
+}
